48651fe32fe4bbdf72fa1e1fa3ceacde488ff5a8
Victor Marotta <victordossantosmarotta@gmail.com>
2024-06-07 06:28:25 -0300

    chore: add pipeline to dockerhub

---
+++ release.config.js
@@ -0,0 +1,31 @@
+const timestamp = new Date()
+  .toISOString()
+  .replace(/[-:]/g, "")
+  .replace(/\..+/, "");
+
+module.exports = {
+  branches: [
+    { name: "main", channel: "latest" },
+    { name: "develop", channel: "nightly", prerelease: `nightly-${timestamp}` },
+  ],
+  repositoryUrl: "https://github.com/paschendale/chimaera-api",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: false,
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["./package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
