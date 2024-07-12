429eae4df913a021ebebe3f39bdd0389f607108b
Victor Marotta <victordossantosmarotta@gmail.com>
2024-07-12 06:40:48 -0300

    refact: optimize db query

--- src/controllers/stats.ts
+++ src/controllers/stats.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from "express";
 import { statsSchema } from "../schema/stats";
-import bairros from "../mocks/bairros";
-import focos from "../mocks/focos";
 import statsServicePontenova from "../services/stats";
 
 export default async function statsController(req: Request, res: Response) {
@@ -15,5 +13,7 @@ export default async function statsController(req: Request, res: Response) {
     const result = await statsServicePontenova(data.data!);
 
     res.send(result);
-  } catch (error) {}
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 }
--- src/services/stats.ts
+++ src/services/stats.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
-import { statsSchema } from "../schema/stats";
 import bairros from "../mocks/bairros";
 import focos from "../mocks/focos";
 import client from "../repositories/db";
+import { statsSchema } from "../schema/stats";
 
 const db = client;
 
@@ -10,104 +10,90 @@ export default async function statsServicePontenova(
   params: z.infer<typeof statsSchema>
 ) {
   try {
-    const totalFocosQuery = await db.query(
-      `
-      SELECT COUNT(*) AS total_focos
-      FROM data.focos
-      WHERE data BETWEEN $1 AND $2
-      AND geom && ST_MakeEnvelope($3, $4, $5, $6, 4326);
-    `,
-      [params.start, params.end, params.swX, params.swY, params.neX, params.neY]
+    const startDateLastYear = new Date(
+      params.start.getFullYear() - 1,
+      params.start.getMonth(),
+      params.start.getDate()
+    );
+    const endDateLastYear = new Date(
+      params.end.getFullYear() - 1,
+      params.end.getMonth(),
+      params.end.getDate()
     );
 
-    const totalFocos = totalFocosQuery.rows[0].total_focos;
-
-    const mediaFocosQuery = await db.query(
-      `
-      SELECT AVG(focos_por_dia) AS media_focos_dia
-      FROM (
-          SELECT DATE(data) AS dia, COUNT(*) AS focos_por_dia
-          FROM data.focos
-          WHERE data BETWEEN $1 AND $2
-          AND geom && ST_MakeEnvelope($3, $4, $5, $6, 4326)
-          GROUP BY DATE(data)
-      ) subquery;
-    `,
-      [params.start, params.end, params.swX, params.swY, params.neX, params.neY]
-    );
-
-    const mediaFocos = mediaFocosQuery.rows[0].media_focos_dia;
-
-    const aumentoMesmoPeriodoQuery = await db.query(
-      `
-      WITH focos_ano_corrente AS (
-        SELECT COUNT(*) AS total_focos
+    const combinedQuery = `
+      WITH focos_periodo AS (
+        SELECT 
+          DATE(data) AS dia, 
+          COUNT(*) AS focos_por_dia,
+          TO_CHAR(data, 'YYYY-MM') AS mes
+        FROM data.focos
+        WHERE geom && ST_MakeEnvelope($3, $4, $5, $6, 4326)
+        GROUP BY dia, mes
+      ),
+      focos_corrente AS (
+        SELECT 
+          COUNT(*) AS total_focos_corrente
         FROM data.focos
         WHERE data BETWEEN $1 AND $2
         AND geom && ST_MakeEnvelope($3, $4, $5, $6, 4326)
       ),
-      focos_ano_anterior AS (
-        SELECT COUNT(*) AS total_focos
+      focos_anterior AS (
+        SELECT 
+          COUNT(*) AS total_focos_ano_anterior
         FROM data.focos
-        WHERE data BETWEEN $3 AND $4
+        WHERE data BETWEEN $7 AND $8
         AND geom && ST_MakeEnvelope($3, $4, $5, $6, 4326)
+      ),
+      media_focos AS (
+        SELECT
+          AVG(focos_por_dia) AS media_focos_dia
+        FROM focos_periodo
+        WHERE dia BETWEEN $1 AND $2
+      ),
+      mes_maior_focos AS (
+        SELECT
+          mes,
+          COUNT(*) AS total_focos_mes
+        FROM focos_periodo
+        GROUP BY mes
+        ORDER BY total_focos_mes DESC
+        LIMIT 1
       )
-      SELECT 
-        (focos_ano_corrente.total_focos - focos_ano_anterior.total_focos) AS aumento,
-        (focos_ano_corrente.total_focos - focos_ano_anterior.total_focos) * 100.0 / focos_ano_anterior.total_focos AS aumento_percentual
-      FROM focos_ano_corrente, focos_ano_anterior;
-    `,
-      [
-        params.start,
-        params.end,
-        new Date(
-          params.start.getFullYear() - 1,
-          params.start.getMonth(),
-          params.start.getDate()
-        ),
-        new Date(
-          params.end.getFullYear() - 1,
-          params.end.getMonth(),
-          params.end.getDate()
-        ),
-        params.swX,
-        params.swY,
-        params.neX,
-        params.neY,
-      ]
-    );
+      SELECT
+        focos_corrente.total_focos_corrente AS total_focos,
+        media_focos.media_focos_dia,
+        (focos_corrente.total_focos_corrente - focos_anterior.total_focos_ano_anterior) AS aumento,
+        (focos_corrente.total_focos_corrente - focos_anterior.total_focos_ano_anterior) * 100.0 / focos_anterior.total_focos_ano_anterior AS aumento_percentual,
+        mes_maior_focos.mes AS mes_com_mais_focos,
+        mes_maior_focos.total_focos_mes AS total_focos_mes
+      FROM focos_corrente, focos_anterior, media_focos, mes_maior_focos;
+    `;
 
-    const aumentoMesmoPeriodoQtde = aumentoMesmoPeriodoQuery.rows[0].aumento;
-    const aumentoMesmoPeriodoPerc =
-      aumentoMesmoPeriodoQuery.rows[0].aumento_percentual;
+    const result = await db.query(combinedQuery, [
+      params.start,
+      params.end,
+      params.swX,
+      params.swY,
+      params.neX,
+      params.neY,
+      startDateLastYear,
+      endDateLastYear,
+    ]);
 
-    const mesComMaiorNumeroQuery = await db.query(
-      `
-      SELECT TO_CHAR(data, 'YYYY-MM') AS mes, COUNT(*) AS total_focos
-      FROM data.focos
-      WHERE data BETWEEN $1 AND $2
-      AND geom && ST_MakeEnvelope($3, $4, $5, $6, 4326)
-      GROUP BY TO_CHAR(data, 'YYYY-MM')
-      ORDER BY total_focos DESC
-      LIMIT 1;
-    `,
-      [params.start, params.end, params.swX, params.swY, params.neX, params.neY]
-    );
-
-    const mesComMaiorNumero = mesComMaiorNumeroQuery.rows[0].mes;
-    const mesComMaiorNumeroQtde = mesComMaiorNumeroQuery.rows[0].total_focos;
+    const stats = result.rows[0];
 
     return {
       meta: params,
       bairros: bairros,
       focos: focos,
       stats: {
-        totalFocos,
-        mediaFocos,
-        aumentoMesmoPeriodoPerc,
-        aumentoMesmoPeriodoQtde,
-        mesComMaiorNumero,
-        mesComMaiorNumeroQtde,
+        totalFocos: stats.total_focos,
+        mediaFocos: stats.media_focos_dia,
+        aumentoMesmoPeriodoPerc: stats.aumento_percentual,
+        aumentoMesmoPeriodoQtde: stats.aumento,
+        mesComMaiorNumero: stats.mes_com_mais_focos,
+        mesComMaiorNumeroQtde: stats.total_focos_mes,
       },
     };
   } catch (error) {
