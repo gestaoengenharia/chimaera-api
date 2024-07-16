6aab9bc44aaed17e3bd534c405d1c2fb30be6f71
Victor Marotta <victordossantosmarotta@gmail.com>
2024-07-16 06:02:16 -0300

    chore: add error handling to api

--- src/services/stats.ts
+++ src/services/stats.ts
@@ -121,6 +121,7 @@ export default async function statsServicePontenova(
     };
   } catch (error) {
     return {
+      error: error,
       meta: params,
       bairros: bairros,
       focos: [],
